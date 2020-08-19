8752ee38ae4fd81e0aec0439282aeb7ab07447c8
6110110592 <6110110592@psu.ac.th>
2020-08-19 22:29:06 +0700

    part 6

--- src/WordCard.js
+++ src/WordCard.js
@@ -9,7 +9,10 @@ const prepareStateFromWord = given_word =>{
     let chars = _.shuffle(Array.from(word))
     return {
         word,
-        chars
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
     }
 }
 
@@ -19,12 +22,26 @@ export default function WordCard(props){
 
     const activationHandler = c => {
         console.log(`${c} has been activated`)
+
+        let guess = state.guess + c
+        setState({...state, guess})
+        
+        if(guess.length == state.word.length){
+            if(guess == state.word){
+                console.log('yeah!')
+                setState({...state, completed: true})
+            }else{
+                console.log('reset, next attempt')
+                setState({...state, guess: '', attempt: state.attempt + 1})
+            }
+        }
+        console.log(guess)
     }
     return (
         <div>
             {
                 state.chars.map((c, i) => 
-                    <CharacterCard value={c} key={i} activationHandler={activationHandler}
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}
                 />)
             }
         </div>
